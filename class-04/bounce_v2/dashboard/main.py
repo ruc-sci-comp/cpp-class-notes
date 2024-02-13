b0283a0b808f2c961de0bbf91b9127c529b77237
N. Sanchirico <Lnk2past@gmail.com>
2024-02-13 17:51:41 +0000

    bounce v2

---
+++ class-04/bounce_v2/dashboard/main.py
@@ -0,0 +1,112 @@
+"""Bounce!
+"""
+import copy
+import io
+import pathlib
+import subprocess
+
+import holoviews as hv
+import hvplot.pandas
+import numpy as np
+import pandas as pd
+import panel as pn
+
+hv.extension('bokeh')
+pn.extension()
+
+
+main = str(pathlib.Path(__file__).parents[1] / 'bin' / 'main.exe')
+
+def load_data():
+    proc = subprocess.run([main], capture_output=True)
+    stdout = proc.stdout.decode()
+    return pd.read_csv(io.StringIO(stdout), names=['t', 'id', 'x', 'y', 'v'], delimiter=' ')
+
+state = load_data()
+state['magnitudes'] = np.abs(state.v) * 0.1
+state['angles'] = np.sign(state.v) * np.pi / 2.0
+time_steps = state.t.unique()
+state = state.groupby('t')
+
+discrete_player = pn.widgets.DiscretePlayer(
+    name='Discrete Player',
+    options=time_steps.tolist(),
+    value=0,
+    interval=33
+)
+
+@pn.depends(discrete_player)
+def plot(value):
+    time_box.value = str(value)
+
+    data = state.get_group(value)
+
+    scatter = hv.Scatter(data, kdims=['x'], vdims=['y', 'id']).opts(
+        size=10,
+        color='id',
+        colorbar=False,
+        cmap='RdYlGn',
+    )
+
+    vector = hv.VectorField(data, kdims=['x', 'y'], vdims=['angles', 'magnitudes', 'id']).opts(
+        rescale_lengths=False,
+        arrow_heads=False,
+        pivot='tail',
+        magnitude=hv.dim('magnitudes'),
+        color='id',
+        colorbar=False,
+        cmap='RdYlGn',
+    )
+    return scatter * vector
+
+point_dmap = hv.DynamicMap(plot).opts(
+    show_grid=True,
+    frame_height=600,
+    frame_width=600,
+    ylim=(-1,101)
+)
+
+time_box = pn.widgets.TextInput(name='time (s)', disabled=True)
+
+fps_input = pn.widgets.IntSlider(name='FPS', start=1, end=30, step=1, value=30)
+
+sim_data_card = pn.Card(
+    time_box,
+    collapsible=False,
+    title='Simulation Data'
+)
+
+sim_config_card = pn.Card(
+    fps_input,
+    collapsible=False,
+    title='Simulation Controls'
+)
+
+description_pane = pn.pane.Markdown('''
+
+### Inputs & Controls
+
+* Enable Trace - turns on a simple movement trace
+* FPS - frames per second, or how quickly we playback the simulation
+
+### Usage
+
+* Click *Play* to start the simulation; then *Stop* to end the simulation. 
+* Optionally, set the FPS to change the speed of the simulation.
+''')
+
+pn.template.FastListTemplate(
+    theme=pn.template.DarkTheme,
+    header_background='#DC143C',
+    accent_base_color='#708090',
+    site='Bounce',
+    title='Multiple Entities Bouncing!',
+    main=[
+        'Simulating multiple entities experiencing inelastic collisions with the ground.',
+        pn.Row(pn.Column(point_dmap, discrete_player), description_pane),
+    ],
+    sidebar=[
+        sim_data_card,
+        sim_config_card
+    ]
+).servable()
---
+++ class-04/bounce_v2/src/main.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <vector>  // this gives us access to the vector data type
+
+using namespace std;
+
+// This code simulates 5 balls falling (due to gravity) and bouncing on the floor.
+// The idea is simple: use equations to update the position and velocity of each ball,
+// and whenever the ball "hits" the ground, we make it bounce by flipping the velocity
+// in the other direction. We also simulate the fact that when something bounces it
+// does not go as high as it did before, and do we use a *coefficient of restitution*
+// to dampen the velocity with each bounce.
+// 
+// Last week we created variables x and y to represent the position of a single entity;
+// doing this for multiple entities becomes very inconvenient, as we would need to
+// create multiple sets of variables for each entity, and then write code to update
+// each separately. This gets insane to code when we have more than 2 or 3 entities.
+//
+// In order for us to simulate many entities at once, we pack their data into vectors.
+// By placing the x values, y values, and velocity values in vectors, we can now use
+// a loop to simulate all of the entities, rather than explicitly coding the equations
+// for each entity.
+// 
+auto main() -> int
+{
+    // In order to simulate the physics of the balls falling, we need to establish time.
+    // We will increment our simulation in 0.1 second intervals
+    auto delta_time = double{ 0.1 };
+
+    // We want to create balls at different coordinates. We start with two vectors, one
+    // for the x and one for the y components of their coordinates. Below we want to create
+    // 5 balls at the following coordinates:
+    //
+    // b1: (0, 60)
+    // b2: (1, 70)
+    // b3: (2, 80)
+    // b4: (3, 90)
+    // b5: (4, 100)
+    //
+    auto xs = vector<double>{ 0, 1, 2, 3, 4 };
+    auto ys = vector<double>{ 60, 70, 80, 90, 100 };
+
+    // Every ball is initially stationary, floating in the air. This means they
+    // all have a velocity of 0!
+    auto vs = vector<double>{ 0, 0, 0, 0, 0 };
+
+    // Every ball is subjected to gravity, so we can say the acceleration of each
+    // ball is -9.81 m/s/s. This is constant for all balls, so we need only a double.
+    auto acceleration = double{ -9.81 };
+
+    // This is the coefficient of restitution. We will set it to 0.8, which means
+    // that after each bounce, the ball that bounced will retain 80% of its kinetic
+    // energy.
+    auto coeff_of_restitution = double{ 0.8 };
+
+    // This is our main simulation loop. We will simulate the passage of time by
+    // creating a variable named "time" set to 0, and we will continue simulating
+    // until 100 seconds have passed. Every time we advance the simulation, it will
+    // move forward by only 0.1 seconds (which is the value of delta_time) 
+    for (auto time = double{}; time < 100.0; time += delta_time)
+    {
+        // Every time we move time forward, we need to move the entities forward through
+        // time. Because all of the data is in a vector, we can readily use a for-loop to
+        // apply the same equations to all entities. This is much better than writing the
+        // same block of code 5 time!
+        //
+        // All of our vectors are the same size, so we can just use one of them to set up
+        // the loop. Here i represents which ball we are currently updating.
+        for (auto i = size_t{}; i < xs.size(); ++i)
+        {
+            // First let's pull out the data associated with the current ball we are simulating
+            auto x = xs[i];
+            auto y = ys[i];
+            auto v = vs[i];
+
+            // This is where things get tricky. In order for us to simulate the bounce, we need
+            // to ensure that the ball does not go through the ground. Since the balls will be
+            // incrementally moving, it is very possible that an increment would make its altitude
+            // (the y-coordinate of position) less than 0. Therefore, we first need to calcuate
+            // how far down the ball is falling - our delta y, or just dy.
+            auto dy = v * delta_time;
+
+            // We need to check if the new position (y plus dy) is at or under ground. If it then
+            // we need to make sure that the collision with the ground is simulated properly. So
+            // if we anticipate falling through the ground (which we cannot do!)...
+            if (y + dy <= 0.0)
+            {
+                // We need to figure out how long it will take to hit the ground from the current
+                // position. We know how high the ball is and how fast it is moving, so dividing
+                // that distance by the speed will give us the time to impact (which is less than
+                // 0.1 seconds!). 
+                auto time_before_bounce = y / v;
+
+                // Once we know how long it will take to hit the ground, we can subtract it from
+                // the original delta time to calculate how much time to simulate after the bounce.
+                // Every step of the simulation needs to ultimately be 0.1 seconds, so all we are
+                // doing here is breaking that 0.1 seconds into a "before bounce" and "after bounce".
+                auto time_after_bounce = delta_time - time_before_bounce;
+
+                // With both sections of time calcuated, we can now apply Euler integration to figure
+                // out how high the ball will bounce. First we need to figure out exactly how fast
+                // the ball is moving when it hits the ground.
+                auto velocity_at_impact = v + acceleration * time_before_bounce;
+
+                // Once we know how fast it is moving when it hits the ground, we can apply the
+                // coefficient of restitution to the velocity to determine how fast it is going
+                // after the bounce. We make the impact velocity negative, so as to "turn" the
+                // ball around.
+                v = -velocity_at_impact * coeff_of_restitution;
+
+                // Now that we know how fast the ball is moving after the bounce, we can simply
+                // apply Euler integration here to calculate how high the bounce is.
+                y = v * time_after_bounce;
+
+                // Lastly, we can apply gravity to the ball's velocity.
+                v += acceleration * time_after_bounce;
+            }
+            else
+            {
+                // If we are not bouncing, we can very easily just apply Euler integration.
+                y += v * delta_time;
+                v += acceleration * delta_time;
+            }
+
+            // Once we have the final y and v values, we just throw them right back into the
+            // vetor, overwriting what was there previously.
+            ys[i] = y;
+            vs[i] = v;
+
+            // Log the data - we log i here so that we can know what data is associated with
+            // which ball.
+            cout << time << " " << i << " " << x << " " << y << " " << v << endl;
+        }
+    }
+}
