021903ccdeed0fd1e37cbf18370a58543572e0d5
N. Sanchirico <Lnk2past@gmail.com>
2024-04-15 20:00:47 +0000

    Prep MC Model

--- class-12/auto-defense/mc_dashboard/main.py
+++ class-12/auto-defense/mc_dashboard/main.py
@@ -8,20 +8,20 @@ import numpy as np
 import pandas as pd
 import panel as pn
 
-from .model import Model
+from .mc_model import MonteCarloModel
 
 hv.extension('bokeh')
 pn.extension()
 
 configuration_file = pathlib.Path(__file__).parents[1] / 'data' / 'config.json'
 configuration = json.load(open(configuration_file))
-model_state = Model()
+model_state = MonteCarloModel()
 
 def plot(data):
-    projectile_plot = hv.Points(data, vdims=['vx', 'vy']).opts(size=10, tools=['hover'])
+    projectile_plot = hv.Points(data, kdims=['x', 'y'], vdims=['vx', 'vy', 'mc']).opts(color='mc', cmap='spectral').redim.range(mc=(0,mc_count_input.value))
     return projectile_plot
 
-dfstream = hv.streams.Pipe(data=pd.DataFrame({'x':[], 'y':[]}))
+dfstream = hv.streams.Buffer(data=pd.DataFrame({'x':[], 'y':[], 'vx':[], 'vy':[], 'mc': []}), length=50000)
 dmap = (hv.DynamicMap(plot, streams=[dfstream])).opts(
     xlim=(0,8500),
     ylim=(0,2000),
@@ -32,52 +32,49 @@ dmap = (hv.DynamicMap(plot, streams=[dfstream])).opts(
 
 periodic_callback = None
 
+mc_count_input = pn.widgets.IntInput(name='Number of Monte Carlo Iterations', start=1, end=100, value=50)
 play_button = pn.widgets.Button(name='Play')
 reset_button = pn.widgets.Button(name='Reset')
-time_box = pn.widgets.TextInput(name='time (s)', disabled=True)
 fps_input = pn.widgets.IntSlider(name='FPS', start=1, end=30, step=1, value=30)
 
-def run_model():
-    global global_time
-    model_state.update(configuration['delta_time'])
-    projectile_state = pd.DataFrame([[*p.position, *p.velocity] for p in model_state.projectiles], columns=['x', 'y', 'vx', 'vy'])
-    time_box.value = f'{model_state.time:.2f}'
-    dfstream.send(projectile_state)
-    if model_state.simulation_complete:
-        periodic_callback.stop()
-        # play_button.clicks += 1
+MC = 0
 
+def run_model():
+    global MC
+    model_state.execute_model(MC)
+    projectile_state = pd.DataFrame([[*p.position, *p.velocity] for p in model_state.mc_data[MC]], columns=['x', 'y', 'vx', 'vy'])
+    projectile_state['mc'] = MC
+    dfstream.send(projectile_state)
+    MC += 1
+    if MC >= mc_count_input.value:
+        play(None)
+        MC = 0
 
 @pn.depends(play_button, watch=True)
 def play(value):
     global periodic_callback
     if periodic_callback is None or not periodic_callback.running:
+        reset(None)
         play_button.name = 'Stop'
-        periodic_callback = pn.state.add_periodic_callback(run_model, period=1000//int(fps_input.value))
+        periodic_callback = pn.state.add_periodic_callback(run_model, period=1000//int(fps_input.value), count=mc_count_input.value)
     elif periodic_callback.running:
         play_button.name = 'Play'
         periodic_callback.stop()
 
 @pn.depends(reset_button, watch=True)
 def reset(value):
+    global periodic_callback
     if periodic_callback:
-        play_button.name = 'Play'
         periodic_callback.stop()
-    model_state.finalize()
-    global configuration 
-    configuration = json.load(open(configuration_file))
-    model_state.initialize(str(configuration_file))
-    projectile_state = pd.DataFrame([[*p.position, *p.velocity] for p in model_state.projectiles], columns=['x', 'y', 'vx', 'vy'])
-    time_box.value = f'{model_state.time:.2f}'
-    dfstream.send(projectile_state)
-
-sim_data_card = pn.Card(
-    time_box,
-    collapsible=False,
-    title='Simulation Data'
-)
+        periodic_callback = None
+        play_button.name = 'Play'
+    model_state.clear()
+    dfstream.clear()
+    global MC
+    MC = 0
 
 sim_config_card = pn.Card(
+    mc_count_input,
     fps_input,
     reset_button,
     play_button,
@@ -97,7 +94,6 @@ pn.template.FastListTemplate(
         dmap
     ],
     sidebar=[
-        sim_data_card,
         sim_config_card
     ]
 ).servable()
--- class-12/auto-defense/src/environment.cpp
+++ class-12/auto-defense/src/environment.cpp
@@ -10,19 +10,19 @@ using namespace std::numbers;
 // coefficient of drag of a spherical object
 constexpr auto CD = double{ 0.47 };
 
-auto Environment::get_drag(const Projectile& projectile) const -> std::array<double, 2>
+auto Environment::get_drag(const Projectile&) const -> std::array<double, 2>
 {
     // need to compute drag!
     return { 0.0, 0.0 };
 }
 
-auto Environment::get_air_density(double a) const -> double
+auto Environment::get_air_density(double) const -> double
 {
     // need to compute the air density at the given altitude!
     return 0.0;
 }
 
-auto Environment::get_gravity(double a) const -> double
+auto Environment::get_gravity(double) const -> double
 {
     // need to compute the gravity at the given altitude!
     return -9.81;
