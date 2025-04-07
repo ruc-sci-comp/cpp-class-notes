1b0722234742fc701b8a0663e049b43ff052cbad
My Name <165396297+Lucas-Liona@users.noreply.github.com>
2025-04-07 23:55:17 +0000

    test

--- class-07/mining/src/worker.cpp
+++ class-07/mining/src/worker.cpp
@@ -1,5 +1,13 @@
 auto work(Mineral &mineral) -> void {
-    //once the entity is close enough, they call work()
+    //once the entity is close enough and the thing has minerals, they call work()
+    int request_amount = worker.capacity - worker.units;
 
+    if (mine.units > 0) 
+    {
+        int amount_available = mineral.mine(request_amount); //can exceed the amount available but this will be handled seperately
+        worker.units += amount_available;
+    }
+
+    return;
     //we reqest as much as we can for increment entity inventory
 };
