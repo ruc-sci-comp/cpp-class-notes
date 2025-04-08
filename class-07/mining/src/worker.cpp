bf36876d586182ee4bf7d57e4f45a239e5d9f37a
My Name <165396297+Lucas-Liona@users.noreply.github.com>
2025-04-08 00:16:52 +0000

    removed Worker. accessor

--- class-07/mining/src/worker.cpp
+++ class-07/mining/src/worker.cpp
@@ -2,12 +2,12 @@
 
 auto Worker::work(Mineral& mineral) -> void {
     //once the entity is close enough and the thing has minerals, they call work()
-    auto request_amount = int {Worker.capacity - Worker.units};
+    auto request_amount = int {capacity - units};
 
     if (mine.units > 0)
     {
         auto amount_available = int {mineral.mine(request_amount)}; //can exceed the amount available but this will be handled seperately
-        Worker.units += amount_available;
+        units += amount_available;
     }
 
     return;
