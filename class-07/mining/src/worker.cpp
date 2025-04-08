9cdb293287a4965cab26e3cdce2da0427d066050
Mauro J. Sanchirico III <156733573+mauro-j-sanchirico@users.noreply.github.com>
2025-04-08 00:58:30 +0000

    Merged row 1 changes

--- class-07/mining/src/worker.cpp
+++ class-07/mining/src/worker.cpp
@@ -1,15 +1,30 @@
-#include "worker.h" 
+#include "mineral.h"
+#include "worker.h"
+#include <cmath>
 
-auto Worker::work(Mineral& mineral) -> void {
-	//once the entity is close enough and the thing has minerals, they call work()
-	auto request_amount = int{ capacity - units };
 
-	if (mine.units > 0)
+
+auto Worker::move(double target_x, double target_y, double speed) -> void
+{
+	Mineral min;
+	auto angle = double{ std::atan2(target_y - min.position_y, target_x - min.position_x) };
+	min.position_x += speed * cos(angle);
+	min.position_y += speed * sin(angle);
+}
+
+auto Worker::update(Mineral& mineral) -> void
+{
+	Mineral min;
+	if (is_empty() && mineral.is_available() && !is_close_to_mineral(min))
 	{
-		auto amount_available = int{ mineral.mine(request_amount) }; //can exceed the amount available but this will be handled seperately
-		units += amount_available;
+		move(min.position_x, min.position_y, 1.0);
 	}
-
-	return;
-	//we reqest as much as we can for increment entity inventory
-};
+	else if (is_empty() && mineral.is_available() && is_close_to_mineral(min))
+	{
+		work(min);
+	}
+	else if (is_full() || !mineral.is_available())
+	{
+		move(0, 0, 1.0);
+	}
+}
