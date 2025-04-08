1010b33b233c4c23e416ce75872b8842a3d1a939
Mauro J. Sanchirico III <156733573+mauro-j-sanchirico@users.noreply.github.com>
2025-04-08 00:55:33 +0000

    Send it!

--- class-07/mining/src/worker.cpp
+++ class-07/mining/src/worker.cpp
@@ -1,15 +1,15 @@
 #include "worker.h" 
 
 auto Worker::work(Mineral& mineral) -> void {
-    //once the entity is close enough and the thing has minerals, they call work()
-    auto request_amount = int {capacity - units};
+	//once the entity is close enough and the thing has minerals, they call work()
+	auto request_amount = int{ capacity - units };
 
-    if (mine.units > 0)
-    {
-        auto amount_available = int {mineral.mine(request_amount)}; //can exceed the amount available but this will be handled seperately
-        units += amount_available;
-    }
+	if (mine.units > 0)
+	{
+		auto amount_available = int{ mineral.mine(request_amount) }; //can exceed the amount available but this will be handled seperately
+		units += amount_available;
+	}
 
-    return;
-    //we reqest as much as we can for increment entity inventory
+	return;
+	//we reqest as much as we can for increment entity inventory
 };
