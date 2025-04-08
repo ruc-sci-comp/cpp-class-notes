f57a21ed9c7bb57d801f26f28c3b0858e4930b35
Mauro J. Sanchirico III <156733573+mauro-j-sanchirico@users.noreply.github.com>
2025-04-08 01:15:59 +0000

    Class code working!

--- class-07/mining/src/worker.cpp
+++ class-07/mining/src/worker.cpp
@@ -6,27 +6,41 @@
 
 auto Worker::move(double target_x, double target_y, double speed) -> void
 {
-	Mineral min;
-	auto angle = double{ std::atan2(target_y - min.position_y, target_x - min.position_x) };
-	min.position_x += speed * cos(angle);
-	min.position_y += speed * sin(angle);
+    // MJS: this moves the worker, not the mineral
+    // We have the worker position so we only need to move the worker,
+    // not the mineral.
+    // Mineral min;
+    auto angle = double{ std::atan2(target_y - position_y, target_x - position_x) };
+    position_x += speed * cos(angle);
+    position_y += speed * sin(angle);
 }
 
 auto Worker::update(Mineral& mineral) -> void
 {
-	Mineral min;
-	if (is_empty() && mineral.is_available() && !is_close_to_mineral(min))
-	{
-		move(min.position_x, min.position_y, 1.0);
-	}
-	else if (is_empty() && mineral.is_available() && is_close_to_mineral(min))
-	{
-		work(min);
-	}
-	else if (is_full() || !mineral.is_available())
-	{
-		move(0, 0, 1.0);
-	}
+    // MJS: didn't need local variable since we have a reference to variable
+    // from the caller's scope
+    // Mineral min;
+    if (is_empty() && mineral.is_available() && !is_close_to_mineral(mineral))
+    {
+        move(mineral.position_x, mineral.position_y, 1.0);
+    }
+    // MJS: remember we have to mine until we are NOT full
+    // full is not the opposite of empty here... we can be half full!
+    else if (!is_full() && mineral.is_available() && is_close_to_mineral(mineral))
+    {
+        work(mineral);
+    }
+    else if (is_full() || !mineral.is_available())
+    {
+        // MJS: Added code to make worker slower with more minerals
+        move(0, 0, 1.0 / (1.0 + units));
+
+        // MJS: added code to zero units
+        if (auto distance_to_origin = std::hypot(position_x, position_y); distance_to_origin < 1e-8)
+        {
+            units = 0;
+        }
+    }
 }
 
 auto Worker::work(Mineral& mineral) -> void
